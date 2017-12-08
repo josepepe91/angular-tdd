39eb8d090ead03138edffcd3456a05e2e0a8b2f1
José Albarado <jalbarado741@gmail.com>
2017-12-07 23:45:08 -0500

    002 - first commit - second test

--- src/app/app.component.spec.ts
+++ src/app/app.component.spec.ts
@@ -1,33 +1,27 @@
+import { ComponentFixture, TestBed } from '@angular/core/testing';
+import { By }              from '@angular/platform-browser';
+import { DebugElement }    from '@angular/core';
+
 import { AppComponent } from './app.component';
 
-import { async, ComponentFixture, TestBed } from '@angular/core/testing';
-import { By }           from '@angular/platform-browser';
-import { DebugElement } from '@angular/core';
+describe('AppComponent (component test)', () => {
 
-describe('AppComponent', function () {
-  let de: DebugElement;
-  let comp: AppComponent;
+  let comp:    AppComponent;
   let fixture: ComponentFixture<AppComponent>;
-
-  beforeEach(async(() => {
-    TestBed.configureTestingModule({
-      declarations: [ AppComponent ]
-    })
-    .compileComponents();
-  }));
+  let de:      DebugElement;
+  let el:      HTMLElement;
 
   beforeEach(() => {
-    fixture = TestBed.createComponent(AppComponent);
-    comp = fixture.componentInstance;
+    TestBed.configureTestingModule({
+      declarations: [ AppComponent ], // declare the test component
+    });
+
+    fixture = TestBed.createComponent(AppComponent); // Instance... Do not re-configure TestBed after calling createComponent.
+
+    comp = fixture.componentInstance; // BannerComponent test instance
+
+    // query for the title <h1> by CSS element selector
     de = fixture.debugElement.query(By.css('h1'));
-  });
-
-  it('should create component', () => expect(comp).toBeDefined() );
-
-  it('should have expected <h1> text', () => {
-    fixture.detectChanges();
-    const h1 = de.nativeElement;
-    expect(h1.innerText).toMatch(/angular/i,
-      '<h1> should say something about "Angular"');
+    el = de.nativeElement;
   });
 });
