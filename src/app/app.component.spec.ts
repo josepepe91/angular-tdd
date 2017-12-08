d3e9b26c3fb9b9f25290d7188183ecd567f42935
José Albarado <jalbarado741@gmail.com>
2017-12-07 23:48:38 -0500

    003 - test - second test

--- src/app/app.component.spec.ts
+++ src/app/app.component.spec.ts
@@ -1,19 +1,19 @@
-import { ComponentFixture, TestBed } from '@angular/core/testing';
-import { By }              from '@angular/platform-browser';
-import { DebugElement }    from '@angular/core';
+import {ComponentFixture, TestBed} from '@angular/core/testing';
+import {By} from '@angular/platform-browser';
+import {DebugElement} from '@angular/core';
 
-import { AppComponent } from './app.component';
+import {AppComponent} from './app.component';
 
 describe('AppComponent (component test)', () => {
 
-  let comp:    AppComponent;
+  let comp: AppComponent;
   let fixture: ComponentFixture<AppComponent>;
-  let de:      DebugElement;
-  let el:      HTMLElement;
+  let de: DebugElement;
+  let el: HTMLElement;
 
   beforeEach(() => {
     TestBed.configureTestingModule({
-      declarations: [ AppComponent ], // declare the test component
+      declarations: [AppComponent], // declare the test component
     });
 
     fixture = TestBed.createComponent(AppComponent); // Instance... Do not re-configure TestBed after calling createComponent.
@@ -23,5 +23,15 @@ describe('AppComponent (component test)', () => {
     // query for the title <h1> by CSS element selector
     de = fixture.debugElement.query(By.css('h1'));
     el = de.nativeElement;
+  });
+  it('should display original name', () => {
+    fixture.detectChanges();
+    expect(el.textContent).toContain(comp.name);
+  });
+
+  it('should display a different test name', () => {
+    comp.name = 'Maria';
+    fixture.detectChanges();
+    expect(el.textContent).toContain('Maria');
   });
 });
