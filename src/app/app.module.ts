e939956c3205b38f2eefb4c778ede47e80912c7b
José Albarado <jalbarado741@gmail.com>
2017-12-10 21:25:31 -0500

    005 - test with service dependency - third test

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,11 +1,15 @@
-import { NgModule }      from '@angular/core';
-import { BrowserModule } from '@angular/platform-browser';
+import {NgModule} from '@angular/core';
+import {BrowserModule} from '@angular/platform-browser';
 
-import { AppComponent }  from './app.component';
+import {AppComponent} from './app.component';
+import {WelcomeComponent} from './welcome.component';
+import {UserService} from './model/user.service';
 
 @NgModule({
-  imports:      [ BrowserModule ],
-  declarations: [ AppComponent ],
-  bootstrap:    [ AppComponent ]
+  imports: [BrowserModule],
+  providers: [UserService],
+  declarations: [AppComponent, WelcomeComponent],
+  bootstrap: [AppComponent]
 })
-export class AppModule { }
+export class AppModule {
+}
